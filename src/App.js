4fec1bed89e3a319a327e32ddf29654425c60a15
Matheus Rodrigues <matheusrodrigues@MacBook-Pro-de-Matheus.local>
2019-10-01 12:52:49 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [test, setTest] = useState(0)
+
+  /**
+   *  Existe dois tipos de useEffect,
+   *  Primeiro - aquele que necessita de fazer uma limpeza como cancelar requisições ou desativar um listening
+   *  Segundo - Aquele que apenas usa do efeito sem necessitar de limpeza
+   */
+
+  //Caso não altere nenhum state você pode usar o useEffect apenas na montagem e demonstagem do componente
+  useEffect(() => {
+    console.log('didMount')
+  }, [])
+
+  // Similar ao componentDidMount e componentDidUpdate:
+  useEffect(() => {
+
+    (count === 5) ? setTest(1) : setTest(0)
+
+    // Atualiza o titulo do documento usando a API do browser
+    document.title = `Você clicou ${count} vezes`;
+
+  }, [count]);
+
+  //Exemplo de efeito com limpeza (retornamos uma função)
+  useEffect(() => {
+    if (test === 0)
+      return
+
+    //A função vai ser executada após este efeito ser chamado novamente, ou seja, ele não é executado na mesma hora e sim após!
+    return () => {
+      setCount(0);
+    };
+
+  }, [test]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
